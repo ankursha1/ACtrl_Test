83f170b6ab2d3853951d3e0ce73cb6af7fff0520
ankursha1 <ankursha1@hotmail.com>
2016-07-17 02:05:10 +0900

    Add home page

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 var express = require('express'),
-	logger = require('./AccessControlApp/util/logger'),
-	bodyParser = require('body-parser'),
-	cookieParser = require('cookie-parser'),
-	methodOverride = require('method-override'),
-	path = require('path'),
-	approutes = require('./AccessControlApp/approutes/routes'),
-	envconst = require('./AccessControlApp/env/constants'),
-	dbconn = require('./AccessControlApp/db/dbconn.js');
+logger = require('./AccessControlApp/util/logger'),
+bodyParser = require('body-parser'),
+cookieParser = require('cookie-parser'),
+methodOverride = require('method-override'),
+path = require('path'),
+approutes = require('./AccessControlApp/approutes/routes'),
+envconst = require('./AccessControlApp/env/constants'),
+dbconn = require('./AccessControlApp/db/dbconn.js');
 
 var server = function() {
 	var app = express();
 	logger.debug('Starting setting up server - ' + __dirname);
 	app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({'extended':'false'}));
-    app.use(methodOverride());
+	app.use(bodyParser.urlencoded({'extended':'false'}));
+	app.use(methodOverride());
 	app.use(cookieParser());
-    var router = express.Router();
-    approutes(app);
+	var router = express.Router();
+	approutes(app);
 	app.use('/', router);
 	
 	app.use(express.static(path.join(__dirname, '/'))); 
 
-    app.get('/', function( req, res, next ) {
-    	res.sendFile(__dirname +'/public/index.html');
-    });
-    
+	app.get('/', function( req, res, next ) {
+		res.sendFile(__dirname +'/public/index.html');
+	});
+	
 	logger.info('Intializing db connection...');
 	dbconn(envconst.DB_INFO);
 
 	logger.info('Finished setting up server.....')
 
-    return app;
+	return app;
 }
 
 module.exports = server;
